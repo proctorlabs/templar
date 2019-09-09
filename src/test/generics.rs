13ac12af7c0cc61f0efd1f043190f20c1c68a053
Phil Proctor <philliptproctor@gmail.com>
2019-09-09 00:24:49 -0400

    Testing updates, split tests into categories

---
+++ src/test/generics.rs
@@ -0,0 +1,29 @@
+use super::*;
+
+#[test]
+fn call_generic_function() -> Result<()> {
+    let mut builder = TemplarBuilder::default();
+    builder
+        .add_generic_function("sum", |args: Vec<i64>| {
+            Ok(args.iter().fold(0, |acc, i| acc + i))
+        })
+        .add_generic_function("add_two_args", |args: (i64, i64)| Ok(args.0 + args.1));
+    let templar = builder.build();
+    let template = templar.parse_expression("sum([5,add_two_args(5,3),3])")?;
+    let context = StandardContext::new(Document::Unit);
+    let res: i64 = template.exec(&context)?.try_into().unwrap();
+    assert_eq!(res, 16 as i64);
+    Ok(())
+}
+
+#[test]
+fn call_generic_filter() -> Result<()> {
+    let mut builder = TemplarBuilder::default();
+    builder.add_generic_filter("add_single", |inc: i64, arg: i64| Ok(inc + arg));
+    let templar = builder.build();
+    let template = templar.parse_expression("5 + 3 | add_single(2)")?;
+    let context = StandardContext::new(Document::Unit);
+    let res: i64 = template.exec(&context)?.try_into().unwrap();
+    assert_eq!(res, 10 as i64);
+    Ok(())
+}
---
+++ src/test/parsing.rs
@@ -0,0 +1,23 @@
+use super::*;
+
+use std::convert::TryInto;
+
+const BASIC: &str = r#"---
+somedict:
+  val1: value
+  val2: "{{ file('README.md') | split | index(0) }}"
+"#;
+
+#[test]
+fn parse_yml_template() -> Result<()> {
+    let template = Templar::global().parse_yaml(BASIC)?;
+    let context = StandardContext::new({
+        let mut doc = Document::default();
+        doc["one"]["two"]["three"] = "val".into();
+        doc
+    });
+    let tmpl: Template = template.get_path(&["somedict", "val2"]).try_into()?;
+    let result = tmpl.render(&context)?;
+    println!("Result: {}", result);
+    Ok(())
+}
