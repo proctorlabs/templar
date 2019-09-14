4f50fa7446c39031a6ded119ba19b7254d0e3a7e
Phil Proctor <philliptproctor@gmail.com>
2019-09-13 21:59:23 -0400

    Lots of documentation updates

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,8 @@ of the currently supported features.
     * Whitespace control can be added to one or both sides of the tags. All spaces, new lines, or other whitespace on the side with the `-`
       on it will be removed as if the block is immediately next to the other element.
 
+As documentation is still in progress, see the [kitchen sink](./examples/kitchen_sink.tmpl) for examples of template usage.
+
 # Expression syntax
 
 Everything inside the standard `{{ }}` block is an expression. Each block holds exactly one expression, but that expression can be chained with
@@ -77,6 +79,8 @@ many individual operations. A quick overview:
 * String concatenation: `~` e.g. `{{ 'Hello' ~ ' ' ~ 'world!' }}` prints "Hello world!"
 * Functions: `ident()` e.g. `{{ env('USER') }}` would retrieve the value of the environment variable "USER".
 * Filters: `|` e.g. `{{ 'hello world' | upper }}` would use the 'upper' filter to print "HELLO WORLD"
+
+As documentation is still in progress, see the [expression tests](./src/test/expressions.rs) for examples of expression usage.
 
 # Performance
 
