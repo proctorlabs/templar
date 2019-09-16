baa887d48e7ef1c9cc7be87d38669255e2726f83
Phil Proctor <philliptproctor@gmail.com>
2019-09-15 22:50:58 -0400

    Added some documentation for filters and functions

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,6 +1,29 @@
 /*!
-# Filters
- */
+Templar filters are used to process the result of an expression in a template.
+
+# Overview
+
+As an example, the expression `{{ 'hello' | upper }}` uses the "upper" filter to create
+the upper case result "HELLO".
+
+# Built in filters
+
+- require: Will throw an error if the result is empty or null
+- default(any): Replaces empty, null, or error types with the default value from the args
+- length: Returns the length of a string or array
+- lower: Lowercase the rendered result
+- upper: Uppercase the rendered result
+- trim: Trim whitespace off the rendered result
+- split(str?): Split a string into an array. Delimited by newline, but an arg can be used to override the delimiter.
+- index(int): Retrieve the int index from the array
+- join(str?): Join an array with the provided string. Defaults to newline
+- string: Forces the result into a string type, usually by rendering it
+- key(str): Retrieve the value of the specified key from the dictionary
+- escape_html: <alias 'e'> Render the result and escape HTML characters
+- yaml: <alias yml> Serialize the data into a YAML string. Requires"yaml-extension" feature (default on)
+- json(str?): Serialize the data into a JSON string. Set str to 'pretty' to print with indentation. Requires the "json-extension" feature (default on)
+- base64(str?): Encode the result as Base64. If the optional string parameter is set to "decode" then it will try to decode instead. Requires "base64-extension" feature (default on)
+*/
 
 mod common;
 
--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,6 +1,29 @@
 /*!
-# Functions
- */
+Templar functions are used for pulling or creating data from other sources or using other methods.
+
+# Overview
+
+As an example, the file() function will open a file and return the contents as a string. Functions do not require
+any data to work, though most need arguments. Functions can be used as an argument to other functions,
+to filters, or as the base operation. For example, this is a valid expression:
+
+```template
+{ 'filename': 'settings.json', 'content': json(file('settings.json')) } | yml
+```
+
+The above will creat a map with two fields "filename" and "content" with content containing the parsed
+contents the file `settings.json`. Then we pass this map to the filter `yml` to then render
+that map into a serialized YML string.
+
+# Built in functions
+
+- file(str): Open file and read contents to a string
+- env(str): Read the named environment variable
+- script(str): Execute the string as a shell script. Returns a map with keys "stdout", "stderr", "status"
+- command(str, str[]?): Execute the supplied command with the supplied arguments. Returns a map with keys "stdout", "stderr", "status"
+- json(str): Parse the supplied JSON string into a map. Requires "json-extension" feature (default on)
+- yaml(str): <alias yml> Parse the supplied YML string into a map. Requires "yaml-extension" feature (default on)
+*/
 
 mod common;
 
