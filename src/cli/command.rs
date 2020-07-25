2caff1b1c2bb442d9d466e5c4f2ccba0c9bebee3
Phil Proctor <philliptproctor@gmail.com>
2020-07-25 01:10:53 -0400

    Initial impl of recursive templating

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -17,12 +17,25 @@ impl Command {
     rename_all = "kebab_case",
     author,
     about,
-    settings = &[ArgRequiredElseHelp, DeriveDisplayOrder, DisableHelpSubcommand, UnifiedHelpMessage, NextLineHelp]
+    settings = &[ArgRequiredElseHelp, DeriveDisplayOrder, DisableHelpSubcommand, UnifiedHelpMessage]
 )]
 pub struct Command {
-    /// File to parse and load into the templating context
-    #[structopt(short, long, number_of_values = 1, multiple = true)]
-    pub input: Vec<PathBuf>,
+    /// Output to send the result to, defaults to stdout.
+    #[structopt(short = "o", long, parse(from_os_str))]
+    pub destination: Option<PathBuf>,
+
+    /// Template file or directory to process
+    #[structopt(short, long, conflicts_with = "expr")]
+    pub template: Option<PathBuf>,
+
+    /// Evaluate a single expression instead of a full template
+    #[structopt(
+        short,
+        long = "expression",
+        name = "expression",
+        conflicts_with = "template"
+    )]
+    pub expr: Option<String>,
 
     /// File to parse and load into the templating context as a dynamic input
     #[structopt(
@@ -34,21 +47,21 @@ pub struct Command {
     )]
     pub dynamic_input: Vec<PathBuf>,
 
+    /// File to parse and load into the templating context
+    #[structopt(short, long, number_of_values = 1, multiple = true)]
+    pub input: Vec<PathBuf>,
+
     /// Directly set a variable on the context
     #[structopt(short, long, parse(try_from_str = parse_key_val), number_of_values = 1)]
     pub set: Vec<(String, String)>,
 
-    /// Output to send the result to, defaults to stdout. If the template input is a directory, then this is required.
-    #[structopt(short = "o", long, parse(from_os_str))]
-    pub destination: Option<PathBuf>,
+    /// Allow directories to be recursively processed
+    #[structopt(short, long)]
+    pub recursive: bool,
 
-    /// Evaluate a single expression instead of a full template
-    #[structopt(short, long = "expression", name = "expression", conflicts_with = "template")]
-    pub expr: Option<String>,
-
-    /// Template file or directory to process
-    #[structopt(short, long, conflicts_with = "expr")]
-    pub template: Option<PathBuf>,
+    /// Overwrite target if it already exists
+    #[structopt(short, long)]
+    pub force: bool,
 }
 
 /// Parse a single key-value pair
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,6 +3,7 @@ use command::*;
 use context::build_context;
 use std::io::prelude::*;
 use std::path::PathBuf;
+use std::fs::create_dir;
 use templar::Templar;
 use util::*;
 
@@ -11,13 +12,7 @@ mod context;
 mod util;
 
 pub fn run() -> Result<()> {
-    let cmdctx = CommandContext::new(Command::parse()?)?;
-    match (&cmdctx.cmd.expr, &cmdctx.cmd.template) {
-        (Some(ref text), None) => cmdctx.exec_expression(text),
-        (None, Some(ref file)) => cmdctx.exec_path(file),
-        (None, None) => cmdctx.exec_stdin(),
-        _ => unreachable!(), //Command:parse() has these as mutually exclusive
-    }
+    CommandContext::new(Command::parse()?)?.run()
 }
 
 #[derive(Debug)]
@@ -32,21 +27,74 @@ impl CommandContext {
         Ok(CommandContext { cmd, ctx })
     }
 
-    fn exec_expression(&self, text: &str) -> Result<()> {
-        self.render(Templar::global().parse_expression(text)?)
+    fn run(&self) -> Result<()> {
+        match (&self.cmd.expr, &self.cmd.template) {
+            (Some(ref text), None) => self.exec_expression(text),
+            (None, Some(ref file)) => self.exec_path(file),
+            (None, None) => self.exec_stdin(),
+            _ => unreachable!(), //Command:parse() has these as mutually exclusive
+        }
     }
 
     fn exec_path(&self, file: &PathBuf) -> Result<()> {
-        let template_contents = read_file(file)?;
-        self.render(Templar::global().parse_template(&template_contents)?)
+        if file.is_file() {
+            let template_contents = read_file(file)?;
+            self.render_file(Templar::global().parse_template(&template_contents)?)
+        } else if file.is_dir() {
+            if self.cmd.recursive {
+                if self.cmd.destination.is_none()
+                    || !self.cmd.destination.as_ref().unwrap().is_dir()
+                {
+                    Err(TemplarError::RenderFailure(
+                        "When templating a directory, the destination must also be a directory"
+                            .into(),
+                    ))
+                } else {
+                    self.render_recursive(file, self.cmd.destination.as_ref().unwrap())
+                }
+            } else {
+                Err(TemplarError::RenderFailure(
+                    "Recursive flag must be used to template a directory.".into(),
+                ))
+            }
+        } else {
+            Err(TemplarError::RenderFailure("Template not found!".into()))
+        }
+    }
+
+    fn exec_expression(&self, text: &str) -> Result<()> {
+        self.render_file(Templar::global().parse_expression(text)?)
     }
 
     fn exec_stdin(&self) -> Result<()> {
         let template_contents = read_stdin()?;
-        self.render(Templar::global().parse_template(&template_contents)?)
+        self.render_file(Templar::global().parse_template(&template_contents)?)
     }
 
-    fn render(&self, tpl: Template) -> Result<()> {
+    fn render_recursive(&self, src: &PathBuf, dst: &PathBuf) -> Result<()> {
+        if src.is_dir() {
+            if !dst.exists() {
+                create_dir(dst)?;
+            }
+            for entry in src.read_dir()? {
+                let p = entry?.path();
+                let filename = p.file_name().unwrap();
+                let mut newsrc = src.clone();
+                let mut newdst = dst.clone();
+                newsrc.push(filename);
+                newdst.push(filename);
+                self.render_recursive(&newsrc, &newdst)?;
+            }
+            Ok(())
+        } else {
+            let template_contents = read_file(src)?;
+            let tpl = Templar::global().parse_template(&template_contents)?;
+            let output = tpl.render(&self.ctx)?;
+            write_file(dst, &output)
+        }
+    }
+
+    fn render_file(&self, tpl: Template) -> Result<()> {
         let output = tpl.render(&self.ctx)?;
         match self.cmd.destination {
             Some(ref file) => write_file(file, &output),
